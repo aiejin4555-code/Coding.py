ec299f1cbc42ee05a2293cab19300e5403cd7aa9
aiejin4555-code <aiejin4555@gmail.com>
2026-02-06 05:40:42 -0700

    Create LA1.py

---
+++ LA1.py
@@ -0,0 +1,10 @@
+#LA1
+#Write a Python script that takes a numerical input representing years.
+#Convert the years to seconds.
+#Assume 1 year = 365 days.
+
+years = float(input("Enter number of year: "))
+
+years_seconds = years * 365 * 24 * 60 * 60
+
+print (f"Output: {years_seconds}")
